a0fe3c6369a8871bc16746bfade601ae4b728dbc
Temidayo Omoyajowo <temidayoomoyajowo@seamlesshr.com>
2024-08-08 13:33:32 +0100

    update connection

--- students/insert_data.sh
+++ students/insert_data.sh
@@ -2,7 +2,7 @@
 
 # Script to insert data from courses.csv and students.csv into students database
 
-PSQL="psql -X --username=freecodecamp --dbname=students --no-align --tuples-only -c"
+PSQL="psql -X --username=temidayoomoyajowo --dbname=students --no-align --tuples-only -c"
 echo $($PSQL "TRUNCATE students, majors, courses, majors_courses")
 
 cat courses.csv | while IFS="," read MAJOR COURSE; do
